7e12cc18645aa4eb0989d7821ec43b8c07132d10
Luis Carlin <luiscarlin@gmail.com>
2018-04-29 09:22:39 -0400

    spike to get google api civics working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'AIzaSyC63iYNVwNtcZ5e_6-fr2X5kjlSANRPXXc'
+const address = "2861 Mondavi Lane, Hilliiard, OH USA"
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      output:  {}
+    }
+  }
+
+  loadYoutubeApi() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      window.gapi.load('client', () => {
+        window.gapi.client.setApiKey(API_KEY);
+
+        var electionId = 2000;
+
+        var req = window.gapi.client.request({
+            'path' : '/civicinfo/v2/voterinfo',
+            'params' : {'electionId' : electionId, 'address' : address}
+        });
+       req.execute((results) => {
+         console.log('results', results)
+         this.setState = {
+           output: results
+         }
+       })
+      })
+    }
+    document.body.appendChild(script);
+  }
+
+  componentDidMount() {
+    this.loadYoutubeApi();
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,9 +53,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          {JSON.stringify(this.state.output)}
+        </p>
       </div>
     );
   }
 }
+
 
 export default App;
