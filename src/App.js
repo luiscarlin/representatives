6806fb0125e3f9267a15c08f2352d010259a12a2
Luis Carlin <luiscarlin@gmail.com>
2018-05-03 05:56:01 -0400

    Fix failing tests

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { fetchRepresentatives } from './services/fetchRepresentatives'
-
-const API_KEY = 'AIzaSyC63iYNVwNtcZ5e_6-fr2X5kjlSANRPXXc'
 
 class App extends Component {
-  loadYoutubeApi() {
-    const script = document.createElement("script");
-    script.src = "https://apis.google.com/js/client.js";
-
-    script.onload = () => {
-      window.gapi.load('client', () => {
-        window.gapi.client.setApiKey(API_KEY);
-
-        fetchRepresentatives().then(console.log)
-      })
-    }
-    document.body.appendChild(script);
-  }
-
-  componentDidMount() {
-    this.loadYoutubeApi();
-  }
-
   render() {
     return (
       <div className="App">
