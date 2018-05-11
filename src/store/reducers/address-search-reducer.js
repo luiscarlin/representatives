9400170901e336ab619a7b0bb38de560982e38d5
Luis Carlin <luiscarlin@gmail.com>
2018-05-11 05:55:17 -0400

    Store is setup

--- src/store/reducers/address-search-reducer.js
+++ src/store/reducers/address-search-reducer.js
@@ -5,6 +5,8 @@ export default (state = DEFAULT_STATE.addressSearch, action) => {
   switch (action.type) {
     case actionTypes.addressSearch.RESET_STATE:
       return Object.assign({}, DEFAULT_STATE.addressSearch)
+    case actionTypes.addressSearch.SET_ADDRESS:
+      return Object.assign({}, state, { address: action.value })
     default:
       return state
   }
