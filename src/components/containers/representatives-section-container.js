05c2a628a46bf2a2870eab095ae60ad0a42a3f0a
Luis Carlin <luiscarlin@gmail.com>
2018-05-16 06:30:06 -0400

    representatives section container tested

--- src/components/containers/representatives-section-container.js
+++ src/components/containers/representatives-section-container.js
@@ -3,7 +3,7 @@ import representativesSection from '../presenters/representatives-section'
 import selectors from '../../store/selectors'
 
 const mapStateToProps = state => ({
-  representatives: selectors.representatives.getRepresentatives()
+  representatives: selectors.representatives.getRepresentatives(state)
 })
 
 export default connect(mapStateToProps)(representativesSection)
--- src/components/containers/representatives-section-container.test.js
+++ src/components/containers/representatives-section-container.test.js
@@ -1,5 +1,20 @@
 import RepresentativesSectionContainer from './representatives-section-container'
+import RepresentativesSection from '../presenters/representatives-section'
 
 describe('Representatives Section Container', () => {
+  const state = {
+    representatives: {
+      data: [
+        {
+          name: 'Dr Who',
+          phone: '893'
+        }
+      ]
+    }
+  }
 
+  it('renders Representative Section Presenter', () => {
+    const mountedComponent = mountComponentWithState(<RepresentativesSectionContainer />, state)
+    expect(mountedComponent.node.find(RepresentativesSection).length).toEqual(1)
+  })
 })
