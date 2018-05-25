ed577dd80f19b6b5839e240eb3f2c1988afcbfa2
Luis Carlin <luiscarlin@gmail.com>
2018-05-25 07:31:49 -0400

    Adding styled components

--- src/components/presenters/representatives-section.js
+++ src/components/presenters/representatives-section.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Card from './generic/card'
+import styled from 'styled-components'
+
+const CardsSection = styled.div`
+  display: flex;
+  flex-flow: wrap;
+`
 
 const representativeCards = (representatives) => (
   representatives.map((rep, index) => <Card person={rep} key={index} />)
 )
 
 export default ({ representatives }) => (
-  <div>
+  <CardsSection>
     { representativeCards(representatives) }
-  </div>
+  </CardsSection>
 )
