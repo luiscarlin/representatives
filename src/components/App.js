159e874d29c1e9515dd5f9875d3b7d90efe805cd
Luis Carlin <luiscarlin@gmail.com>
2018-05-27 09:36:32 -0400

    style the page better

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import AddressSearchSection from './containers/address-search-section-container'
 import RepresentativesSection from './containers/representatives-section-container'
+import styled from 'styled-components'
+
+const App = styled.div`
+  background-color: #f3f2ee;
+  height: 100vh;
+  width: 100vw;
+`
+
+const Body = styled.div`
+  max-width: 1370px;
+  background-color: white;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 40px 30px;
+`
 
 export default () => (
-  <div>
-    <AddressSearchSection />
-    <RepresentativesSection />
-  </div>
+  <App>
+    <Body>
+      <AddressSearchSection/>
+      <RepresentativesSection/>
+    </Body>
+  </App>
 )
