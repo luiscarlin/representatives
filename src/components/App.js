b5febda2a17b97f50289d8dfeccd07ccd45791ed
Luis Carlin <luiscarlin@gmail.com>
2018-05-28 10:01:46 -0400

    change style of body

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,32 @@ import RepresentativesSection from './containers/representatives-section-contain
 import styled from 'styled-components'
 
 const App = styled.div`
-  background-color: #f3f2ee;
+  background-color: #2e323a;
   height: 100vh;
   width: 100vw;
 `
 
 const Body = styled.div`
   max-width: 1370px;
-  background-color: white;
   margin-left: auto;
   margin-right: auto;
   padding: 40px 30px;
 `
 
+const AddressSearchWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50%;
+
+`
+
 export default () => (
   <App>
     <Body>
-      <AddressSearchSection/>
+      <AddressSearchWrapper>
+        <AddressSearchSection/>
+      </AddressSearchWrapper>
       <RepresentativesSection/>
     </Body>
   </App>
