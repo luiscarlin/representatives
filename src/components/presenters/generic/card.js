f386c72d316692fa4b107eebd4bbf42d4f538189
Luis Carlin <luiscarlin@gmail.com>
2018-06-20 07:54:01 -0400

    Add all info to one card and start styling it

--- src/components/presenters/generic/card.js
+++ src/components/presenters/generic/card.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import './card.scss'
 
-export default ({ person }) => (
-  <div className='card'>
-    <img className='image' src={person.photoUrl} alt={person.name}/>
-    <div>
-      <div className='name'>{person.name}</div>
-      <div className='email'>{person.email}</div>
-      <div className='phone'>{person.phone}</div>
-      <div className='address'>{person.address}</div>
-    </div>
-  </div>
-)
+export default ({ person }) => {
+  const { address } = person
+  const cityStateZip = `${address.city} ${address.state}, ${address.zip}`
+
+  return (
+    <figure className='card'>
+      <div className='image'>
+        <img src={person.photoUrl} alt={person.name} />
+      </div>
+      <figcaption className='caption'>
+        <div className='name'>{person.name}</div>
+        <div className='office'>{person.office}</div>
+        <div className='party'>{person.party}</div>
+        <div className='email'>{person.email}</div>
+        <div className='phone'>{person.phone}</div>
+        <div className='address'>
+          <div className='address-name'>{address.name}</div>
+          <div className='address-street'>{address.street}</div>
+          <div className='address-city-state-zip'>{cityStateZip}</div>
+        </div>
+        <div className='social-media'>
+          <i className="fab fa-twitter-square"></i>
+          <i className="fab fa-facebook-square"></i>
+          <i class="fab fa-google-plus-square"></i>
+          <i class="fab fa-youtube-square"></i>
+        </div>
+      </figcaption>
+    </figure>
+  )
+}
--- src/components/presenters/representatives-section.js
+++ src/components/presenters/representatives-section.js
@@ -6,8 +6,27 @@ const representativeCards = (representatives) => (
   representatives.map((rep, index) => <Card person={rep} key={index} />)
 )
 
+const oneRep = () => {
+  const rep = {
+    photoUrl: "https://www.whitehouse.gov/sites/whitehouse.gov/files/images/45/PE%20Color.jpg",
+    office: 'President of the United States',
+    email: 'hello@bye.com',
+    name: "Donald J. Trump",
+    party: 'Republican',
+    phone: "(202) 456-1111",
+    address: {
+      name: 'The WhiteHouse',
+      street: '1600 Pennsylvania Avenue NW',
+      city: 'Washington',
+      state:'DC',
+      zip: '20500'
+    }
+  }
+  return <Card person={rep} />
+}
+
 export default ({ representatives }) => (
   <div className='representatives-section'>
-    { representativeCards(representatives) }
+    { oneRep() }
   </div>
 )
