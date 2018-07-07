5a7c6e9d41be4c89ba185589bd709487cc25f25e
Luis Carlin <luiscarlin@gmail.com>
2018-07-06 20:51:12 -0400

    Add gray color to the background of the whole app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import Store from './store/store'
 
-import './index.css'
+import './index.scss'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
