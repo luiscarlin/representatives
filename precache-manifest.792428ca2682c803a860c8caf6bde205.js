92a0cd2e259eb1f1d5101f78bdcc8c326579d480
Luis Carlin <luiscarlin@gmail.com>
2018-10-22 07:50:41 -0400

    Updates

---
+++ precache-manifest.792428ca2682c803a860c8caf6bde205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a311b1581b50555a70f79a3989c11dd",
+    "url": "/representatives/static/media/capitol.5a311b15.svg"
+  },
+  {
+    "revision": "a96276322732141ab2e8",
+    "url": "/representatives/static/js/runtime~main.a9627632.js"
+  },
+  {
+    "revision": "5d7c5036397afd27a6a1",
+    "url": "/representatives/static/js/main.5d7c5036.chunk.js"
+  },
+  {
+    "revision": "a79d2f31937eab3b5b30",
+    "url": "/representatives/static/js/1.a79d2f31.chunk.js"
+  },
+  {
+    "revision": "5d7c5036397afd27a6a1",
+    "url": "/representatives/static/css/main.b0c43f26.chunk.css"
+  },
+  {
+    "revision": "17356a042e5f4a7be5e95c4be10d3ef7",
+    "url": "/representatives/index.html"
+  }
+];
