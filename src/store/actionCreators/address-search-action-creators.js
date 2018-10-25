58179b0bd100b706fce2c7f451ad570b851e1b09
mdurban <murban117@gmail.com>
2018-10-25 09:05:10 -0400

    add google autocomplete for address search bar

--- src/store/actionCreators/address-search-action-creators.js
+++ src/store/actionCreators/address-search-action-creators.js
@@ -3,7 +3,7 @@ import types from '../actionTypes'
 const setAddress = (address) => {
   return {
     type: types.addressSearch.SET_ADDRESS,
-    value: address
+    value: address.formatted_address
   }
 }
 
