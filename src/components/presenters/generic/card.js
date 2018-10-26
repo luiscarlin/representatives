58f4448591398fb37b373bb0c7dd6f495a9bb3f7
Drew Burns <aburns@pillartechnology.com>
2018-10-26 10:03:08 -0400

    use case statement and handle default case when no social info available

--- src/components/presenters/generic/card.js
+++ src/components/presenters/generic/card.js
@@ -24,24 +24,24 @@ export default ({ person }) => {
       return
     }
     return socialMediaList.map((socialMedia, index) => {
-      if (socialMedia.site === 'GooglePlus') {
-        const hrefValue = `https://plus.google.com/${socialMedia.handle}`
-        return <a href={hrefValue} key={index}><i className='fab fa-google-plus-square' /></a>
-      }
-      else if (socialMedia.site === 'Twitter') {
-        const hrefValue = `https://twitter.com/${socialMedia.handle}`
-        return <a href={hrefValue} key={index}><i className='fab fa-twitter-square' /></a>
-      }
-      else if (socialMedia.site === 'Facebook') {
-        const hrefValue = `https://facebook.com/${socialMedia.handle}`
-        return <a href={hrefValue} key={index}><i className='fab fa-facebook-square' /></a>
-      }
-      else if (socialMedia.site === 'YouTube') {
-        const hrefValue = `https://youtube.com/${socialMedia.handle}`
-        return <a href={hrefValue} key={index}><i className='fab fa-youtube-square' /></a>
-      }
-      else {
-        console.log('Social media', socialMedia.name, 'not recognized')
+      let hrefValue
+
+      switch(socialMedia.site) {
+        case "GooglePlus":
+          hrefValue = `https://plus.google.com/${socialMedia.handle}`
+          return <a href={hrefValue} key={index}><i className='fab fa-google-plus-square' /></a>
+        case "Twitter":
+          hrefValue = `https://twitter.com/${socialMedia.handle}`
+          return <a href={hrefValue} key={index}><i className='fab fa-twitter-square' /></a>
+        case "Facebook":
+          hrefValue = `https://facebook.com/${socialMedia.handle}`
+          return <a href={hrefValue} key={index}><i className='fab fa-facebook-square' /></a>
+        case "YouTube":
+          hrefValue = `https://youtube.com/${socialMedia.handle}`
+          return <a href={hrefValue} key={index}><i className='fab fa-youtube-square' /></a>
+        default:
+          console.log('Social media', socialMedia.name, 'not recognized')
+          return <span></span>
       }
     })
   }
