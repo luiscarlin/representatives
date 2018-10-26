df0f57ef6f505e2ce9da0fcca238cd069e8bc081
Luis Carlin <luiscarlin@gmail.com>
2018-10-26 05:49:48 -0400

    Updates

---
+++ precache-manifest.938debff9d404d1f66a30abe449ef46f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ff364fc0d9f702f2b3b8b2b7cbd9570",
+    "url": "/representatives/static/media/capitol.4ff364fc.svg"
+  },
+  {
+    "revision": "a96276322732141ab2e8",
+    "url": "/representatives/static/js/runtime~main.a9627632.js"
+  },
+  {
+    "revision": "baef65310b87ec07b41d",
+    "url": "/representatives/static/js/main.baef6531.chunk.js"
+  },
+  {
+    "revision": "afe0e6686a39df0c6ee1",
+    "url": "/representatives/static/js/1.afe0e668.chunk.js"
+  },
+  {
+    "revision": "baef65310b87ec07b41d",
+    "url": "/representatives/static/css/main.0e53a467.chunk.css"
+  },
+  {
+    "revision": "f4ad04bdc137f3e24735c8bb2805e2f8",
+    "url": "/representatives/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/representatives/precache-manifest.e34f08e2933e905677ba41b7fa1931d6.js"
+  "/representatives/precache-manifest.938debff9d404d1f66a30abe449ef46f.js"
 );
 
 workbox.clientsClaim();
