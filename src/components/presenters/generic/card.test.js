3bf9f22cd0392f9d6eb32e936b8e12648031a29f
mdurban <murban117@gmail.com>
2018-10-28 20:30:08 -0400

    wrap tests around social media links

--- src/components/presenters/generic/card.test.js
+++ src/components/presenters/generic/card.test.js
@@ -30,7 +30,7 @@ describe('Card', () => {
     expect(subject.find(Img).props().src[1]).toEqual(capitolSvg)
   })
 
-  it('displays the capitor while the person image is loading', () => {
+  it('displays the capitol while the person image is loading', () => {
     expect(subject.find(Img).props().loader).toEqual(<img alt="Pedro P. Pedrozo" src="capitol.svg" />)
   })
 
@@ -48,5 +48,44 @@ describe('Card', () => {
 
   it('has the address of the person', () => {
     expect(subject.find('.address-lines').text()).toEqual('The WhiteHouse')
+  })
+
+  describe('soical media', () => {
+    beforeEach(() => {
+      person = {
+        socialMedia: [
+          {site: 'GooglePlus', handle: 'googleHandle'},
+          {site: 'Twitter', handle: 'twitterHandle'},
+          {site: 'Facebook', handle: 'facebookHandle'},
+          {site: 'YouTube', handle: 'youtubeHandle'},
+        ]
+      }
+
+      subject = shallow(<Card person={person} />)
+    })
+
+    it('opens GooglePlus links in new tab', () => {
+        const googlePlusLink = subject.find({href: `https://plus.google.com/googleHandle`})
+
+        expect(googlePlusLink.props().target).toEqual('_blank')
+    })
+
+    it('opens Twitter links in new tab', () => {
+        const twitterLink = subject.find({href: `https://twitter.com/twitterHandle`})
+
+        expect(twitterLink.props().target).toEqual('_blank')
+    })
+
+    it('opens Facebook links in new tab', () => {
+        const facebookLink = subject.find({href: `https://facebook.com/facebookHandle`})
+
+        expect(facebookLink.props().target).toEqual('_blank')
+    })
+
+    it('opens YouTube links in new tab', () => {
+      const youtubeLink = subject.find({href: `https://youtube.com/youtubeHandle`})
+
+        expect(youtubeLink.props().target).toEqual('_blank')
+    })
   })
 })
