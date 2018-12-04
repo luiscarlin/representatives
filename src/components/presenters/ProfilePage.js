f9fa730a3767481c9c33668eecab7343084510a1
mdurban <murban117@gmail.com>
2018-12-03 20:45:57 -0500

    initial styling for profile page

--- src/components/presenters/ProfilePage.js
+++ src/components/presenters/ProfilePage.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './profile-page.scss'
+import capitolSvg from "../../assets/capitol.svg";
+import Img from "react-image";
 
 class ProfilePage extends React.Component {
 
   render() {
+    console.log('rep ', this.props.representativeInfo)
+
     const classes = []
     classes.push('profile-modal')
     if (!this.props.shouldDisplayModal) {
@@ -11,7 +15,18 @@ class ProfilePage extends React.Component {
     }
 
     return <div className={classes.join(' ')}>
-      <div className='representative-name'>{this.props.representativeInfo.name}</div>
+        <div className='top-banner'>
+          <div className='info'>
+            <Img
+              src={[this.props.representativeInfo.photoUrl, capitolSvg]}
+              loader={<img src={capitolSvg} alt={this.props.representativeInfo.name}/>}
+            />
+            <div className='name'>{this.props.representativeInfo.name}</div>
+            <div className='office'>{this.props.representativeInfo.office}</div>
+            <div className='party'>{this.props.representativeInfo.party}</div>
+          </div>
+        </div>
+      <div className='division'>{this.props.representativeInfo.division}</div>
     </div>
   }
 }
